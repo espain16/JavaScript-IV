0dd216e33276dcb5c1b5bc74acd34b89018b9259
Elise Spain <elisespain@gmail.com>
2019-04-18 15:48:27 -0700

    instructor complete

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -9,6 +9,24 @@ class Person{
     }
 
 speaks(){
-    return `Hello my name is ${name}, I am from ${location}`
+    return `Hello my name is ${name}, I am from ${location}.`
 }
+}
+
+class Instructor{
+    constructor(person){
+        this.name = Derrick;
+        this.age = person.age;
+        this.location = person.location;
+        this.gender = person.gender;
+        this.specialty = person.specialty;
+        this.favLanguage = person.favLanguage;
+        this.catchPhrase = person.catchPhrase 
+    }
+    demo(){
+       return `Today we are learning about ${this.subject}.`
+    }
+    grade(){
+        return `${student.name}receives a perfect score on ${this.subject}.`
+    }
 }
