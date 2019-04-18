c63844d69b9ea3859f97e7c7f710aa4325879a9e
Elise Spain <elisespain@gmail.com>
2019-04-18 16:09:29 -0700

    added super

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -11,17 +11,16 @@ class Person{
 speaks(){
     return `Hello my name is ${name}, I am from ${location}.`
 }
-}
+}//base-class
 
 class Instructor{
     constructor(person){
-        this.name = Derrick;
-        this.age = person.age;
-        this.location = person.location;
-        this.gender = person.gender;
-        this.specialty = person.specialty;
-        this.favLanguage = person.favLanguage;
-        this.catchPhrase = person.catchPhrase 
+        super(InstructorPerson); 
+            this.specialty = person.specialty;
+            this.favLanguage = person.favLanguage;
+            this.catchPhrase = person.catchPhrase
+        
+        
     }
     demo(){
        return `Today we are learning about ${this.subject}.`
@@ -29,4 +28,11 @@ class Instructor{
     grade(){
         return `${student.name}receives a perfect score on ${this.subject}.`
     }
+}//instructor
+
+class student{
+    constructor(person){
+        
+    }
 }
+
