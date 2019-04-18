198dac4319ac3faa9e312932b915a3593c096cd2
Elise Spain <elisespain@gmail.com>
2019-04-18 15:20:31 -0700

    base-class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,14 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(person){
+        this.name = person.name;
+        this.age = person.age;
+        this.location = person.location;
+        this.gender = person.gender;
+
+    }
+
+speaks(){
+    return `Hello my name is ${name}, I am from ${location}`
+}
+}
